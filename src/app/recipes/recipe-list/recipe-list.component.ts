3ae133aa5cbcd848a50068739a219da07c0b5c3e
Adela Simkova <adela@sherpadevs.com>
2024-05-11 09:57:07 +0200

    recipe component populated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+  ];
 
   ngOnInit() {
   }
