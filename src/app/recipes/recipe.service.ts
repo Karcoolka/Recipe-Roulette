a936515ecc6d9fe5880876ac0de401fd894185dd
Adela Simkova <adela@sherpadevs.com>
2024-05-23 16:32:39 +0200

    add ingredients to the shopping list logic updated

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,43 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.modal";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
-    new Recipe('A Test Recipe 22222', 'This is simply a test', 'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+    new Recipe(
+      'Icebox Cake',
+      'No oven, no problem.',
+      'https://hips.hearstapps.com/hmg-prod/images/del049924-blueberry-cheesecake-icebox-cake-web-217-rv-lead-66393c6454c07.jpg?crop=1.00xw:0.502xh;0,0.189xh&resize=1200:*',
+      [
+        new Ingredient('Graham Crackers', 16),
+        new Ingredient('Heavy Cream', 2),
+        new Ingredient('Powdered Sugar', 1),
+      ]),
+    new Recipe(
+      'Super Salad',
+      'Potato salad is just the beginning.',
+      'https://hips.hearstapps.com/hmg-prod/images/blackberry-peach-salad-vertical-662ad5484c91e.jpg?crop=0.819xw:0.328xh;0.0881xw,0.395xh&resize=1200:*',
+      [
+        new Ingredient('Apples', 5),
+        new Ingredient('Bananas', 3),
+        new Ingredient('Lemon juice', 1),
+      ]),
   ];
+
+  constructor(
+    private slService: ShoppingListService
+  ) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredientsMethod(ingredients);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
 import {Ingredient} from "../shared/ingredient.modal";
+import {EventEmitter} from "@angular/core";
 
 export class ShoppingListService {
+  ingredientChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -12,5 +14,14 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredientsMethod(ingredients: Ingredient[]) {
+    // for(let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientChanged.emit(this.ingredients.slice());
   }
 }
