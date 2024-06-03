68fdfcfced02b409c44e1c372261bfb266f57ecb
Adela Simkova <adela@sherpadevs.com>
2024-06-03 08:18:21 +0200

    http requests to BE firebase configured

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,30 +8,37 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Icebox Cake',
-      'No oven, no problem.',
-      'https://hips.hearstapps.com/hmg-prod/images/del049924-blueberry-cheesecake-icebox-cake-web-217-rv-lead-66393c6454c07.jpg?crop=1.00xw:0.502xh;0,0.189xh&resize=1200:*',
-      [
-        new Ingredient('Graham Crackers', 16),
-        new Ingredient('Heavy Cream', 2),
-        new Ingredient('Powdered Sugar', 1),
-      ]),
-    new Recipe(
-      'Super Salad',
-      'Potato salad is just the beginning.',
-      'https://hips.hearstapps.com/hmg-prod/images/blackberry-peach-salad-vertical-662ad5484c91e.jpg?crop=0.819xw:0.328xh;0.0881xw,0.395xh&resize=1200:*',
-      [
-        new Ingredient('Apples', 5),
-        new Ingredient('Bananas', 3),
-        new Ingredient('Lemon juice', 1),
-      ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Icebox Cake',
+  //     'No oven, no problem.',
+  //     'https://hips.hearstapps.com/hmg-prod/images/del049924-blueberry-cheesecake-icebox-cake-web-217-rv-lead-66393c6454c07.jpg?crop=1.00xw:0.502xh;0,0.189xh&resize=1200:*',
+  //     [
+  //       new Ingredient('Graham Crackers', 16),
+  //       new Ingredient('Heavy Cream', 2),
+  //       new Ingredient('Powdered Sugar', 1),
+  //     ]),
+  //   new Recipe(
+  //     'Super Salad',
+  //     'Potato salad is just the beginning.',
+  //     'https://hips.hearstapps.com/hmg-prod/images/blackberry-peach-salad-vertical-662ad5484c91e.jpg?crop=0.819xw:0.328xh;0.0881xw,0.395xh&resize=1200:*',
+  //     [
+  //       new Ingredient('Apples', 5),
+  //       new Ingredient('Bananas', 3),
+  //       new Ingredient('Lemon juice', 1),
+  //     ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(
     private slService: ShoppingListService
   ) {
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
